e2bbef65f7d78f12be1404c4c65143424d7ed5be
otis22 <pro100print@yahoo.com>
2016-11-22 15:30:34 +0200

    push

--- test.php
+++ test.php
@@ -1,8 +1,19 @@
 <?php
 //Документация http://wiki.vetmanager.ru/index.php/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:REST_API
 require_once './Vetmanager.php';
+
+$vetmanager = new Vetmanager('testarseniy.vetmanager.ru', 'key');
+
+var_dump($vetmanager->request('admission', 1));
+
+/*
 define('NEED_NUMBER_LENGTH', 9);
 // Получили номер, должен быть в формате чистых чисел 4324234
+
+
+
+
+
 
 $number = filter_input(INPUT_GET, 'num');
 //$number = '375447688419';
@@ -35,17 +46,4 @@ $clearPhones = $vetmanager->request(
             )
         )
 );
-
-
-if (is_array($clearPhones) && !empty($clearPhones) && $clearPhones['success'] && $clearPhones['data']['totalCount'] > 0) {
-    try {
-        // Достали ИД клиента
-        $clientID = $clearPhones['data']['clientPhone'][0]['client_id'];
-        // Получили всю информацию о клиенте
-        $client = $vetmanager->request('client', $clientID);
-        // Отдали Имя Фамилию
-        echo $client['data']['client']['first_name']  . " " . $client['data']['client']['last_name'] ; 
-    } catch (Exception $e) {
-        exit();
-    }
-}
+*/
