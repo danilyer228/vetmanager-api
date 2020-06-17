911742ecf4487db03cb5fe5507c44c73cf9f6ec4
otis <pro100print@yahoo.com>
2020-06-17 11:20:23 +0300

    add url class

--- src/Url.php
+++ src/Url.php
@@ -6,5 +6,15 @@ namespace Otis22\VetmanagerApi;
 
 final class Url
 {
+    /**
+     * @var string
+     */
+    private $domain;
+
+    public function __toString(): string
+    {
+        return $this->domain . "/vetmanager.ru";
+    }
+
 
 }
