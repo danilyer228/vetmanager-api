65cb2a2f99e6b646bb4b7b449557b313cc102d48
otis <pro100print@yahoo.com>
2020-06-18 17:18:35 +0300

    for support old php

--- src/Url.php
+++ src/Url.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Otis22\VetmanagerApi;
 
-interface Url extends \Stringable
+interface Url
 {
     /**
      * Need for php older than 7.4v, because __toString generate fatal Error when throw any Exception
      * @return string
      */
     public function asString(): string;
+
+    /**
+     * TODO: Need extends Stringable interface for new php versions
+     * @return string
+     */
+    public function __toString(): string;
 }
